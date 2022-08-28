b7c7d584dc8c74214b8d373391835ae76f77df9b
Matias Flores <49283383+nanoes@users.noreply.github.com>
2022-08-28 05:14:08 -0300

    done

--- Ejercicio4/query.sql
+++ Ejercicio4/query.sql
@@ -2,10 +2,13 @@
     for people who work in departments where
     the sum of the salaries of the employees that comprise them is greater than
     250,000. */
-use evalart;
-select lastname, salary from APPX_employee
-INNER JOIN appx_educationlevel 
-ON APPX_employee.educationlevel_id = appx_educationlevel.id 
- where salary >  25000000
-order by lastname;
 
+use evalart;
+select
+    lastname,
+    appx_educationlevel.descriptionn
+from
+    APPX_employee  INNER JOIN appx_educationlevel ON APPX_employee.educationlevel_id = appx_educationlevel.id
+where
+    salary > 250 order by
+    lastname;
